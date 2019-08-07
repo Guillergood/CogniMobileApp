a0b13b72143a421d035484d55a0b39cb017fbec5
Guillermo Bueno Vargas <guillergood@gmail.com>
2019-08-07 17:19:42 +0200

    Hecho de manera más manejable
    Signed-off-by: Guillermo Bueno Vargas <Guillergood@gmail.com>

--- app/src/main/java/ugr/gbv/myapplication/fragments/DrawTask.java
+++ app/src/main/java/ugr/gbv/myapplication/fragments/DrawTask.java
@@ -1,20 +1,23 @@
-package ugr.gbv.myapplication.tests;
+package ugr.gbv.myapplication.fragments;
 
-import android.app.Activity;
 import android.app.Dialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
-import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.constraintlayout.widget.ConstraintSet;
+import androidx.fragment.app.Fragment;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -25,26 +28,24 @@ import ugr.gbv.myapplication.utilities.AV;
 import ugr.gbv.myapplication.utilities.DrawingView;
 import ugr.gbv.myapplication.utilities.Point;
 
+public class DrawTask extends Fragment {
 
-//TODO ESTO VA A TENNER QUE SER UN FRAGMENT EL CUAL VA A ESTAR EN UNA ACTIVIDAD DE TEST
-public class DrawTask extends Activity {
-
+    private Context context;
+    private View view;
     private DrawingView drawingView;
     private ArrayList<Point> secuence;
     private ArrayList<Point> answer;
 
+    @Nullable
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.draw_task);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        //getActionBar().hide();
-        drawingView = findViewById(R.id.drawingSpace);
+        view = inflater.inflate(R.layout.draw_task,container, false);
+        drawingView = view.findViewById(R.id.drawingSpace);
 
+        context = getContext();
 
-
-        Button nextButton = findViewById(R.id.nextTaskButton);
+        Button nextButton = view.findViewById(R.id.nextTaskButton);
         nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -57,7 +58,7 @@ public class DrawTask extends Activity {
             }
         });
 
-        FloatingActionButton helpButton = findViewById(R.id.helpButton);
+        FloatingActionButton helpButton = view.findViewById(R.id.helpButton);
         helpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -67,14 +68,18 @@ public class DrawTask extends Activity {
 
 
 
-        Button prevButtonQUITAR = findViewById(R.id.prevButtonQuitar);
+        Button prevButtonQUITAR = view.findViewById(R.id.prevButtonQuitar);
         prevButtonQUITAR.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Dialog builder = new Dialog(DrawTask.this);
+                Dialog builder = new Dialog(context);
                 builder.requestWindowFeature(Window.FEATURE_NO_TITLE);
-                builder.getWindow().setBackgroundDrawable(
-                        new ColorDrawable(android.graphics.Color.TRANSPARENT));
+                Window window = builder.getWindow();
+                if(window != null) {
+                    window.setBackgroundDrawable(
+                            new ColorDrawable(android.graphics.Color.TRANSPARENT));
+                }
+
                 builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
                     @Override
                     public void onDismiss(DialogInterface dialogInterface) {
@@ -82,7 +87,7 @@ public class DrawTask extends Activity {
                     }
                 });
 
-                ImageView imageView = new ImageView(DrawTask.this);
+                ImageView imageView = new ImageView(context);
                 imageView.setImageDrawable(getResources().getDrawable(R.drawable.cube));
                 imageView.setBackgroundColor(getResources().getColor(R.color.white));
                 //set the image in dialog popup
@@ -99,15 +104,18 @@ public class DrawTask extends Activity {
 
 
         answer = new ArrayList<>();
+
+
+        return view;
     }
 
     private void drawButtons(ArrayList<Point> points) {
-        ConstraintLayout layout = findViewById(R.id.drawTaskLayout);
+        ConstraintLayout layout = view.findViewById(R.id.drawTaskLayout);
         ConstraintSet set = new ConstraintSet();
         set.clone(layout);
 
         for (Point point:points) {
-            Button button = new Button(this);
+            Button button = new Button(context);
             button.setId(View.generateViewId());
             button.setBackground(getResources().getDrawable(R.drawable.circle_no_fill));
             button.setText(point.getLabel());
@@ -128,12 +136,13 @@ public class DrawTask extends Activity {
                 }
             });
             layout.addView(button);
+            int dimens = getResources().getInteger(R.integer.circle);
+            int halfDimen = dimens/2;
             set.connect(button.getId(), ConstraintSet.LEFT, ConstraintSet.PARENT_ID, ConstraintSet.LEFT, 0);
             set.connect(button.getId(), ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.TOP, 0);
-            //TODO CAMBIAR PARA QUE ESTEN EN DIMENSIONES
-            set.setTranslation(button.getId(),point.getX()-48, point.getY()-48);
-            set.constrainHeight(button.getId(), 96);
-            set.constrainWidth(button.getId(), 96);
+            set.setTranslation(button.getId(),point.getX()-halfDimen, point.getY()-halfDimen);
+            set.constrainHeight(button.getId(), dimens);
+            set.constrainWidth(button.getId(), dimens);
 
 
             set.applyTo(layout);
@@ -141,5 +150,8 @@ public class DrawTask extends Activity {
         }
 
     }
+
+
+
 
 }
